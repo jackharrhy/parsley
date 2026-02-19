3cb6127548a81746e169b6c84c282eb6ac7cbb2d
Jack Arthur Harrhy <me@jackharrhy.com>
2026-02-18 23:40:34 -0330

    remove Prefixes func

--- parser.go
+++ parser.go
@@ -302,11 +302,6 @@ func (parser *Parser) GetCommands() []CommandDetails {
 	return commandDetails
 }
 
-// Prefixes returns the prefixes the parser is configured with.
-func (parser *Parser) Prefixes() []string {
-	return parser.prefixes
-}
-
 // New creates a new Parsley parser.
 func New(prefixes ...string) *Parser {
 	return &Parser{
